3379961cec9edb7c501471a43c6772511eb3a581
abhiiiijain <abhinandanbansal123@gmail.com>
2025-02-04 14:36:36 +0530

    clan tags added

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
 const warlogRoutes = require("./routes/warlog");
+const clanTagRoutes = require("./routes/clanTagRoutes");
+
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api", warlogRoutes);
+app.use("/api/clantags", clanTagRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
