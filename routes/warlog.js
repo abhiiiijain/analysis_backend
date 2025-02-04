4913faf65250764097bd2f97188a6ab39e189cb6
abhiiiijain <abhinandanbansal123@gmail.com>
2025-02-04 16:44:10 +0530

    warlog sliced to 7

--- routes/warlog.js
+++ routes/warlog.js
@@ -29,7 +29,7 @@ router.post("/warlog", async (req, res) => {
         .status(404)
         .json({ error: "No warlog data found for this clan." });
     }
-    res.json(warlog.slice(0, 10));
+    res.json(warlog.slice(0, 7));
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
